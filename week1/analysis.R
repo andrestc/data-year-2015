df011a23fdc4d9621580f356a882fd9543bda2e3
Andre Carvalho <andre.carvalho@hotelurbano.com.br>
2015-01-07 22:02:04 -0200

    Week 1 problem 2 solution.

--- week1/analysis.R
+++ week1/analysis.R
@@ -8,5 +8,10 @@ data <- read.csv("./data/bike_trip_data.csv")
 # Problem 1: What was the average total time (in minutes) used by a bicycle in the data?
 # The Duration column seems to be in seconds (analysing start and end dates)
 averageMinsDuration <- mean(data$Duration / 60)
+paste("Problem 1:", averageMinsDuration)
 
 # Problem 2: What was the most popular day by trip frequency in this dataset?
+datesCounts <- table(as.Date(data$Start.Date, format = "%m/%d/%y")) # convert to a count table of dates
+maxCount <- max(datesCounts) # get max date
+mostFrequent <- names(datesCounts[ datesCounts == maxCount ]) # get the name of the day with most counts
+paste("Problem 2:", mostFrequent)
